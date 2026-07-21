cf52e1c7506e8df535df1601804888b3663180b7
nsanso97 <nsansoni97@gmail.com>
2026-07-21 05:49:17 +0200

    update

--- .bashrc.d/alias.sh
+++ .bashrc.d/alias.sh
@@ -1,5 +1,8 @@
 alias sudo="sudo "
-alias git-lc="git ls-files | xargs -n1 git blame --line-porcelain | sed -n 's/^author //p' | sort -f | uniq -ic | sort -nr"
 alias ls="ls -a --color=auto "
 alias ll="ls -lah "
 alias xc="xclip -sel c "
+
+alias glc="git ls-files | xargs -n1 git blame --line-porcelain | sed -n 's/^author //p' | sort -f | uniq -ic | sort -nr"
+alias gbr="git branch -r --sort=-committerdate --format='%format='%(refname:short)%09%(committerdate:relative)%09%(authorname)%09(contents:subject)' | column -t -s $'\t' "
+alias gl="git log --pretty=format:'%Cblue%h%Creset%x09%s%x09%Cred%ar%Creset%x09%Cgreen%an%Creset' "
